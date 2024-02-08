1dd9818e1aabd576031afc32e4ebc3926503b50f
Gabriele <gabriele.palma.bool@gmail.com>
2024-02-08 16:01:07 +0100

    create project laravel (first)

---
+++ resources/views/hello.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Link css style-->
+    <link rel="stylesheet" href="../css/app.css">
+ 
+    <title>hello world laravel</title>
+</head>
+<body>
+    
+    <a href="https://www.youtube.com/watch?v=7XGVdVouTrA&ab_channel=orsidanna"><h1>Hello World</h1></a>
+       
+</body>
+</html>
