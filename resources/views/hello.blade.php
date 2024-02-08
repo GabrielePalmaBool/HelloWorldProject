f3c0d6b520c2df973403889c05fdafa4a8d3447d
Gabriele <gabriele.palma.bool@gmail.com>
2024-02-08 16:06:23 +0100

    fix

--- resources/views/hello.blade.php
+++ resources/views/hello.blade.php
@@ -5,13 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <!-- Link css style-->
-    <link rel="stylesheet" href="../css/app.css">
+    <link rel="stylesheet" href="..\css\app.css">
  
     <title>hello world laravel</title>
 </head>
 <body>
     
-    <a href="https://www.youtube.com/watch?v=7XGVdVouTrA&ab_channel=orsidanna"><h1>Hello World</h1></a>
+    <a href="https://www.youtube.com/watch?v=7XGVdVouTrA&ab_channel=orsidanna" style="color:red; text-align:center;"><h1>Hello World</h1></a>
        
 </body>
 </html>
